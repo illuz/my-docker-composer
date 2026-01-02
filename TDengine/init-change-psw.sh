a5056da2d52eab3f5eeb59a4f5e06953c77ffe5a
illuz <iilluzen@gmail.com>
2026-01-02 22:07:27 +0800

    update tdengine

--- TDengine/init-change-psw.sh
+++ TDengine/init-change-psw.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# 在容器中执行
 
 # 尝试使用默认密码连接并修改为新密码
 # -u root -p taosdata (默认)
 # -s 执行 SQL
-taos -u root -p taosdata -s "ALTER USER root PASS 'YourStrongPassword';"
+taos -u root -p taosdata -s "ALTER USER root PASS 'sjgE0dPg3ySe8MwPwetMAS0W72beMsTf';"
 
 echo "Password updated or already changed."
+
+
+# 或者直接执行：
+# docker exec -it tdengine taos -s "ALTER USER root PASS 'sjgE0dPg3ySe8MwPwetMAS0W72beMsTf';"
